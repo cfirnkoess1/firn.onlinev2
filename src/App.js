aec352d052f296d82603322113b0dcd0328f0cf6
CFirnkoess <Truly@LAPTOP-73S7HVL7>
2023-10-08 23:28:36 -0700

    First real update-added homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <div className = "Background-Image" >
+      {/*br is to lower the heading message*/}
+    <br></br>
+    <br></br>
+      <h1 >Welcome</h1> 
+      <h3>to Firn.Online</h3>
     </div>
+    </body>
   );
 }
 
